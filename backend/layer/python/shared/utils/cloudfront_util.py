a596397598679b1749d791351892ee4c74af3633
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-09-26 18:15:54 -0500

    cloudfront_util.py initial version complete

--- backend/layer/python/shared/utils/cloudfront_util.py
+++ backend/layer/python/shared/utils/cloudfront_util.py
@@ -1,31 +1,40 @@
 import datetime
-from dotenv import load_dotenv
+import os
 
+from dotenv import load_dotenv
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from botocore.signers import CloudFrontSigner
 from secrets_manager_util import SecretManagerUtil
+from dynamodb_util import DynamoDBResourceUtil
 
 load_dotenv()
 
 class CloudFrontSignerService:
     def __init__(self) -> None:
-        self.pem_key = SecretManagerUtil.get_secret('secret-name')
+        pass
 
     def rsa_signer(self, message):
+        secret_manager_util = SecretManagerUtil()
+
         private_key = serialization.load_pem_private_key(
-            self.pem_key,
+            secret_manager_util.get_secret(os.getenv('CF_PRIVATE_KEY_SECRET_NAME')),
             password=None,
             backend=default_backend()
         )
 
         return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
 
-    def generate_signed_url(self, media_id, content_url, public_key_id, expiration_in_seconds):
+    def generate_signed_url(self, media_id: str, expiration_in_seconds: int):
+        ddb_resource_util = DynamoDBResourceUtil()
+        metadata = ddb_resource_util.get_media_metadata(media_id)
+
+        content_url = f'{os.getenv('CLOUDFRONT_DOMAIN')}{metadata['s3_key']}'
+        
         expiration_time = datetime.datetime.now() + datetime.timedelta(seconds=expiration_in_seconds)
 
-        cloudfront_signer = CloudFrontSigner(public_key_id, self.rsa_signer)
+        cloudfront_signer = CloudFrontSigner(os.getenv('CF_PUBLIC_KEY_ID'), self.rsa_signer)
 
         signed_url = cloudfront_signer.generate_presigned_url(
             url=content_url,
@@ -33,3 +42,7 @@ class CloudFrontSignerService:
         )
 
         return signed_url
+    
+cfsigner = CloudFrontSignerService()
+
+print(cfsigner.generate_signed_url('abc123', 3600))

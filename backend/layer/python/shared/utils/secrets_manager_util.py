6e99aff05c5e8e4efbba63fb90513b3700b2b2c9
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-09-26 22:39:52 -0500

    refactoring and modularizing helper and boto3 util functions 1

--- backend/layer/python/shared/utils/secrets_manager_util.py
+++ backend/layer/python/shared/utils/secrets_manager_util.py
@@ -3,8 +3,9 @@ import os
 from botocore.client import BaseClient
 from botocore.exceptions import ClientError
 
+
 class SecretManagerUtil:
-    def __init__(self, region: str = os.getenv('AWS_REGION', 'us-east-2')) -> None:
+    def __init__(self, region: str = os.getenv("AWS_REGION", "us-east-2")) -> None:
         """
         Initializes the Secrets Manager client.
 
@@ -12,19 +13,18 @@ class SecretManagerUtil:
         """
         self.region = region
         self.client: BaseClient = boto3.client(
-            service_name='secretsmanager',
-            region_name=region
+            service_name="secretsmanager", region_name=region
         )
-    
+
     def get_secret(self, secret_name: str) -> bytes:
         try:
             response = self.client.get_secret_value(SecretId=secret_name)
-            secret = response['SecretString']
+            secret = response["SecretString"]
 
             if isinstance(secret, str):
-                secret = secret.encode('utf-8')
+                secret = secret.encode("utf-8")
 
-            return secret.encode('utf-8') if isinstance(secret, str) else secret
-        
+            return secret.encode("utf-8") if isinstance(secret, str) else secret
+
         except ClientError as e:
             raise Exception(f"Failed to retrieve secret: {e}")

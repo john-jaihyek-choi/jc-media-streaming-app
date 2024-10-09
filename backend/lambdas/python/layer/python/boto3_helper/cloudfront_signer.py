50b75e111768f7227ff2508c0b7ddd4332a89137
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-09 01:31:53 -0500

    enhanced code logic and added additional parameter

--- backend/lambdas/python/layer/python/boto3_helper/cloudfront_signer.py
+++ backend/lambdas/python/layer/python/boto3_helper/cloudfront_signer.py
@@ -5,14 +5,14 @@ if os.getenv("AWS_EXECUTION_ENV"):
     sys.path.append("/opt/python")
 
 import datetime, validators
-from custom_utils.logger import logger_config
+from layer.python.custom_utils.logger import logger_config
+from layer.python.custom_utils.exceptions import InvalidSignedUrlError
 from dotenv import load_dotenv
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from botocore.signers import CloudFrontSigner as cfsigner
 from botocore.exceptions import ClientError
-from layer.python.custom_utils.exceptions import InvalidSignedUrlError
 from typing import Optional
 
 # Load env variable
@@ -34,12 +34,13 @@ class CloudFrontSigner:
 
         return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
 
-    def generate_signed_url(
+    def generate_presigned_url(
         self,
         pem_key: bytes,
         public_key_id: str,
         url: str,
         expiration_in_seconds: Optional[int] = os.getenv("CF_DEFAULT_URL_EXP"),
+        policy: Optional[str] = None,
     ) -> str:
         if not pem_key:
             logger.error(
@@ -50,27 +51,28 @@ class CloudFrontSigner:
             logger.error(
                 f"pem_key is not of type 'bytes'. pem_key must be a valid bytes type. Received type(pem_key)={type(pem_key)}"
             )
-            raise ValueError(f"Invalid argumnet value type provided")
+            raise ValueError(f"Invalid argument value type provided")
 
         self.pem_key: bytes = pem_key
 
         expiration_time = datetime.datetime.now() + datetime.timedelta(
-            seconds=expiration_in_seconds
+            seconds=int(expiration_in_seconds)
         )
 
         try:
             cloudfront_signer = cfsigner(public_key_id, self._rsa_signer)
 
             signed_url: str = cloudfront_signer.generate_presigned_url(
-                url=url, date_less_than=expiration_time, policy=None
+                url=url, date_less_than=expiration_time, policy=policy
             )
 
-            if not validators.url(signed_url):
-                raise InvalidSignedUrlError()
+            # Leaving it for potential error handling in the future
+            # if not validators.url(signed_url):
+            #     raise InvalidSignedUrlError()
 
             logger.info("signed url generation successful")
 
-            return signed_url
+            return {"url": signed_url}
 
         except ClientError as e:
             logger.error(f"ClientError occurred: {e.response['Error']}")

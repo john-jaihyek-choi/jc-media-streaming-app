d8b9faa5c3fbdaf074e691c0b10b3d9267643db4
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-09 16:08:41 -0500

    lambda functions added to lambda stack

--- backend/lib/lambda-stack.ts
+++ backend/lib/lambda-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from "constructs";
 import { NestedStack, NestedStackProps } from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export interface LambdaStackProps extends NestedStackProps {
   // custom interface goes here
@@ -37,19 +39,44 @@ export class LambdaStack extends NestedStack {
     });
 
     // Lambda Functions
-    const functionName1 = new lambda.Function(this, "FunctionName1", {
+    const getMedias = new lambda.Function(this, "get_medias", {
       runtime: python3_12_runtime,
-      handler: "main.main",
+      handler: "main.handler",
       code: lambda.Code.fromAsset(
         path.join(__dirname, "../lambdas/python/function/get_medias")
       ),
+      environment: {
+        METADATA_DDB_TABLE_NAME: process.env.METADATA_DDB_TABLE_NAME || "",
+        LOG_LEVEL: process.env.LOG_LEVEL || "",
+      },
       layers: [pythonLayer],
     });
 
-    cdk.Tags.of(functionName1).add(mainStack.stackName, "FunctionName1");
+    cdk.Tags.of(getMedias).add(mainStack.stackName, "get_medias");
 
     new cdk.CfnOutput(this, "FunctionArn", {
-      value: functionName1.functionArn,
+      value: getMedias.functionArn,
+    });
+
+    const getMediaUrl = new lambda.Function(this, "get_media_url", {
+      runtime: python3_12_runtime,
+      handler: "main.handler",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "../lambdas/python/function/get_media_url")
+      ),
+      environment: {
+        CF_PRIVATE_KEY_SECRET_ID: process.env.CF_PRIVATE_KEY_SECRET_ID || "",
+        CF_PUBLIC_KEY_ID: process.env.CF_PUBLIC_KEY_ID || "",
+        CF_DEFAULT_URL_EXP: process.env.CF_DEFAULT_URL_EXP || "",
+        LOG_LEVEL: process.env.LOG_LEVEL || "",
+      },
+      layers: [pythonLayer],
+    });
+
+    cdk.Tags.of(getMediaUrl).add(mainStack.stackName, "get_media_url");
+
+    new cdk.CfnOutput(this, "FunctionArn", {
+      value: getMediaUrl.functionArn,
     });
   }
 }

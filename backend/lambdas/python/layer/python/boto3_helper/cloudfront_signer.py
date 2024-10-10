337cf4814d89cccfc755668d4ca820ca660f056b
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-10 14:18:57 -0500

    cloudfront_signer code refactor

--- backend/lambdas/python/layer/python/boto3_helper/cloudfront_signer.py
+++ backend/lambdas/python/layer/python/boto3_helper/cloudfront_signer.py
@@ -7,7 +7,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from botocore.signers import CloudFrontSigner as cfsigner
-from botocore.exceptions import ClientError
 from typing import Optional
 
 # Load env variable
@@ -18,8 +17,18 @@ logger = logger_config(__name__)
 
 
 class CloudFrontSigner:
-    def __init__(self) -> None:
-        self.pem_key: Optional[bytes] = None
+    def __init__(self, public_key_id: str, pem_key: bytes) -> None:
+        if not pem_key:
+            raise TypeError("missing 1 required positional argument: 'pem_key'")
+        elif not public_key_id:
+            raise TypeError("missing 1 required positional argument: 'public_key_id'")
+        elif not isinstance(pem_key, bytes):
+            raise TypeError(
+                f"pem_key is not of type 'bytes'. pem_key must be a valid bytes type. Received type(pem_key)={type(pem_key)}"
+            )
+        self.pem_key = pem_key
+        self.public_key_id = public_key_id
+        self.cloudfront_signer = cfsigner(public_key_id, self._rsa_signer)
 
     # used exclusively for CloudFrontSigner
     def _rsa_signer(self, message):
@@ -31,33 +40,29 @@ class CloudFrontSigner:
 
     def generate_presigned_url(
         self,
-        pem_key: bytes,
-        public_key_id: str,
         url: str,
-        expiration_in_seconds: Optional[int] = os.getenv("CF_DEFAULT_URL_EXP"),
+        expiration_in_seconds: int = os.getenv("CF_DEFAULT_URL_EXP"),
         policy: Optional[str] = None,
     ) -> str:
-        if not pem_key:
-            logger.error(
-                f"Invalid pem_key value is provided. Received pem_key={pem_key}"
+        if not url:
+            raise TypeError("missing 1 required positional argument: 'url")
+        elif not expiration_in_seconds:
+            raise TypeError(
+                "missing 1 required positional argument: 'expiration_in_seconds"
             )
-            raise ValueError("Invalid argument value is provided.")
-        elif not isinstance(pem_key, bytes):
-            logger.error(
-                f"pem_key is not of type 'bytes'. pem_key must be a valid bytes type. Received type(pem_key)={type(pem_key)}"
+        elif not isinstance(expiration_in_seconds, str) and not isinstance(
+            expiration_in_seconds, int
+        ):
+            raise TypeError(
+                f"expiration_in_seconds must be of type int. Received type {type(expiration_in_seconds)}"
             )
-            raise ValueError(f"Invalid argument value type provided")
-
-        self.pem_key: bytes = pem_key
-
-        expiration_time = datetime.datetime.now() + datetime.timedelta(
-            seconds=int(expiration_in_seconds)
-        )
 
         try:
-            cloudfront_signer = cfsigner(public_key_id, self._rsa_signer)
+            expiration_time = datetime.datetime.now() + datetime.timedelta(
+                seconds=int(expiration_in_seconds)
+            )
 
-            signed_url: str = cloudfront_signer.generate_presigned_url(
+            signed_url: str = self.cloudfront_signer.generate_presigned_url(
                 url=url, date_less_than=expiration_time, policy=policy
             )
 
@@ -66,12 +71,10 @@ class CloudFrontSigner:
             #     raise InvalidSignedUrlError()
 
             logger.info("signed url generation successful")
+            logger.debug(signed_url)
 
             return {"url": signed_url}
 
-        except ClientError as e:
-            logger.error(f"ClientError occurred: {e.response['Error']}")
-            raise RuntimeError("An AWS service client error occurred.")
         except Exception as e:
-            logger.error(f"An unexpected error occurred: {str(e)}")
-            raise RuntimeError("An unexpected AWS service error occurred.")
+            logger.error(f"{e}")
+            raise ValueError(e)

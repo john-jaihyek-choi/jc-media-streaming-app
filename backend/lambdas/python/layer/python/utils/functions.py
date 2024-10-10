8979137d5e3049470a9f58c764ed5aaf015d8e53
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-10 17:45:31 -0500

    file relocation

--- backend/lambdas/python/layer/python/utils/functions.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def generate_api_response(status_code: int, body: dict) -> dict:
-    return {
-        "statusCode": status_code,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps(body),
-    }

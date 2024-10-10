5b17a07ad771c00a49a1487543361f42104f37ba
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-10 18:21:52 -0500

    renamed utils -> jc_custom_utils and boto3_helper to jc_boto3_helper

--- backend/lambdas/python/layer/utils/exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidSignedUrlError(Exception):
-    """Exception raised for errors in CloudFrontSignerHelper."""
-
-    def __init__(self, message="The pre-signed url is invalid"):
-        self.message = message
-        super().__init__(self.message)

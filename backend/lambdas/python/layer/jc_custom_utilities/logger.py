764c0cf1195a1fcced7e4c488118af92e8fd1a65
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-11 00:12:49 -0500

    enhanced logger to accomodate lambda runtime

--- backend/lambdas/python/layer/jc_custom_utilities/logger.py
+++ backend/lambdas/python/layer/jc_custom_utilities/logger.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 import logging
 from logging import Logger
 from dotenv import load_dotenv
@@ -6,19 +6,42 @@ from dotenv import load_dotenv
 # Load env variable
 load_dotenv()
 
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+level_val = getattr(logging, log_level, logging.INFO)
+
+# Set up the logging configuration with the desired format
 config = {
-    "level": os.getenv("LOG_LEVEL"),
+    "level": level_val,
     "format": "|    %(asctime)s    |    %(name)s    |    %(filename)s:%(lineno)s    |\n"
     "%(levelname)s: %(message)s\n",
-    "handlers": [
-        # logging.FileHandler("app.log"),   # If need to log to a file
-        logging.StreamHandler()  # Also logs to console
-    ],
+    "handlers": [logging.StreamHandler()],  # Also logs to console
 }
 
 
 def logger_config(name: str) -> Logger:
-    # Configures basic logging format and handlers
-    logging.basicConfig(**config)
+    logger = logging.getLogger(name)
+    logger.setLevel(level_val)
 
-    return logging.getLogger(name)
+    # Clear any existing handlers to avoid duplicates
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    # Create a stream handler (console)
+    stream_handler = logging.StreamHandler()
+    # Apply the CustomFormatter with your existing format
+    formatter = CustomFormatter(config["format"])
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        # Check if the message is a dictionary or list
+        if isinstance(record.msg, (dict, list)):
+            # Convert it to a JSON string with indentation
+            record.msg = json.dumps(record.msg, indent=4)
+
+        # Use the base Formatter to format everything else
+        return super().format(record)

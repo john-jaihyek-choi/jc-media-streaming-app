98419f407ea88363df1de4a806682983776e9431
john-jaihyek-choi <jaihyek.choi@gmail.com>
2024-10-11 14:39:45 -0500

    layer package script updated

--- backend/lambdas/python/layer/package_layer.sh
+++ backend/lambdas/python/layer/package_layer.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
+echo "Script is located at: $SCRIPT_DIR"
+echo "PROJECT ROOT is located at: $PROJECT_ROOT_DIR"
+
+read -p "Do you confirm? (y/n): " choice
+case "$choice" in 
+  y|Y ) echo "Process starting...";;
+  n|N ) echo "Please verify package_layer.sh for correct path to root and the script"; exit 1;;
+  * ) echo "Process starting...";;
+esac
 
 # Step 1: Generate requirements.txt for boto3_helper and utils
-echo "Generating requirements for boto3_helper/ and utils/..."
-pipreqs ../. --force
+echo "Generating project requirements..."
+poetry export -f requirements.txt --output "${PROJECT_ROOT_DIR}/requirements.txt"
 
 # Step 2: Temporarily create directory for zip
 echo "Creating temporary directory for layer"
-rsync -av --exclude='package_layer.sh' --exclude='*.zip' --exclude='python' ./ python/
+rsync -av --exclude="package_layer.sh" --exclude="*.zip" --exclude="${SCRIPT_DIR}/python" "${SCRIPT_DIR}/" "${SCRIPT_DIR}/python/"
 
 # Step 3: Install dependencies from the generated requirements.txt files
 echo "Installing dependencies from requirements.txt files into python/lib/python3.12/site-packages/..."
-pip install -r ../requirements.txt -t python/lib/python3.12/site-packages/
+pip install -r "${PROJECT_ROOT_DIR}/requirements.txt" -t "${SCRIPT_DIR}/python/lib/python3.12/site-packages/"
 echo "Dependency installation completed!"
 
 # Step 4: Zip content for Lambda Layer
 echo "Zipping the packages for Lambda Layer..."
-zip -r layer_package.zip python/
+# zip -r layer_package.zip python/
+(cd "${SCRIPT_DIR}" && zip -r "layer_package.zip" python/)
 
 # Step 5 Cleaning up temporary directory
-echo "Cleaning up temporary directory python/..."
-rm -r python/
-rm utils/requirements.txt
-rm boto3_helper/requirements.txt
+echo "Cleaning up temporary directories..."
+rm -r "${SCRIPT_DIR}/python/"
+# rm utils/requirements.txt
+# rm boto3_helper/requirements.txt
 
 echo "layer.zip creation successful!"
